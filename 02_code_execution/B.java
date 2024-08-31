5393db5585301ddaced4525d89308775150c88b4
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-08-31 14:57:21 +0530

    Add 2nd folder

---
+++ 02_code_execution/B.java
@@ -0,0 +1,15 @@
+class student {
+
+        String name;
+        int age;
+
+        static int maxAge = 20;
+        
+        public static void main(String[] args){
+
+                student x = new student();
+
+                System.out.println(x.name);
+                System.out.println(x.age);
+        }
+}
