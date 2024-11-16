6920be16e4a1cbff0a7b30d6e5ba78af7e673fbc
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-11-16 16:38:57 +0530

    execution_folder

---
+++ 02_code_execution/A.java
@@ -0,0 +1,9 @@
+class student {
+
+        String name;
+        int age;
+
+        public static void main(String[] args){
+            student x = new student();
+        }
+}
---
+++ 02_code_execution/D.java
@@ -0,0 +1,21 @@
+class D {
+
+        public static void main(String[] agrs){
+            abc();
+        }
+
+        static void abc(){
+
+                bcd();
+        }
+
+        static void bcd(){  
+
+                cde();
+        }
+
+        static void cde(){
+
+                System.out.println('A');
+        }
+}
