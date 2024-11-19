ae487c80cf64ac2553c6b152ba051aeea4e42cbd
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-11-19 17:19:27 +0530

    Codes

---
+++ 7_packages/A/a/b/c/A.java
@@ -0,0 +1,20 @@
+package a.b.c;
+
+class A {
+
+        public static void main(String[] args){
+
+                B x;
+        }
+}
+
+
+
+
+// D:\javaprac\7_packages\A>javac a/b/c/A.java
+// a\b\c\A.java:7: error: cannot find symbol
+//                 B x;
+//                 ^
+//   symbol:   class B
+//   location: class A
+// 1 error
