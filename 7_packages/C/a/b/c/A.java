eeaafff70d8f148c3c0478175d657f7ac46696ba
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-11-20 10:31:13 +0530

    codes

---
+++ 7_packages/C/a/b/c/A.java
@@ -0,0 +1,18 @@
+package a.b.c;
+
+import x.y.z.B;
+
+class A {
+
+        public static void main(String[] args){
+
+                B x ;
+        }
+}
+
+
+
+
+// D:\javaprac\7_packages\C>javac a/b/c/A.java
+
+// D:\javaprac\7_packages\C>java a/b/c/A
