477a674dc6d4d6731cd173008257141c71490a6a
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-11-21 15:52:20 +0530

    import

---
+++ 9_import/I.java
@@ -0,0 +1,22 @@
+// import static java.lang.Math.PI;
+// import static java.lang.Math.E;
+// import static java.lang.Math.max;
+
+//or
+
+import static java.lang.Math.*;
+class I {
+    public static void main(String[] args) {
+        System.out.println(PI);
+        System.out.println(E);
+
+        int max = max(412,89);
+        System.out.println(max);
+    }    
+}
+
+
+// 3.141592653589793
+// 2.718281828459045
+// 412
+
