529d50e01e60d30794d12dfba2fc03154ef9b409
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-11-21 15:50:09 +0530

    import

---
+++ 9_import/H.java
@@ -0,0 +1,18 @@
+import static java.lang.Math.PI;
+import static java.lang.Math.E;
+
+class H{
+
+        public static void main(String[] args){
+
+                System.out.println(PI);
+                System.out.println(E);
+
+                int max = Math.max(66,525);
+                System.out.println(max);
+        }
+}
+
+// 3.141592653589793
+// 2.718281828459045
+// 525
