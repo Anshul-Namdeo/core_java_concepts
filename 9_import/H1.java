7367c5e0ba7ac90ab827d7159654357f7cf047df
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-11-21 15:51:07 +0530

    import

---
+++ 9_import/H1.java
@@ -0,0 +1,21 @@
+import static java.lang.Math.PI;
+import static java.lang.Math.E;
+import static java.lang.Math.max;
+
+class H1 {
+    public static void main(String[] args) {
+        System.out.println(PI);
+        System.out.println(E);
+
+        int max = max(412,89);
+        System.out.println(max);
+    }    
+}
+
+
+
+
+
+3.141592653589793
+2.718281828459045
+412
