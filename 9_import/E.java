d393f010db58c41abdd21935912efd5388bf7361
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-11-21 15:43:36 +0530

    import

---
+++ 9_import/E.java
@@ -0,0 +1,11 @@
+// improt java.lang.*;
+class E {
+
+        public static void main(String[] args){
+
+                System x;
+                String y;
+                Integer z;
+                Float  w;
+        }
+}
