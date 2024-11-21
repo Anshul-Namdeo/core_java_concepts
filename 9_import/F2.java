63bc68d907e865e6edc9ba4f66bccd1334d2f7ee
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-11-21 15:47:53 +0530

    import

---
+++ 9_import/F2.java
@@ -0,0 +1,19 @@
+import java.util.Date;
+import java.io.File;
+
+class F2{
+
+        public static void main(String [] args){
+
+                Date x;
+                File y;
+                Formate z;
+        }
+}
+
+// F2.java:10: error: cannot find symbol
+//                 Formate z;
+//                 ^
+//   symbol:   class Formate
+//   location: class F2
+// 1 error
