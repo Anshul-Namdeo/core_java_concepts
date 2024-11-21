a3f9dc6cbf9631528aaa781cfde46c65c2e327ec
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-11-21 15:48:41 +0530

    import

---
+++ 9_import/G.java
@@ -0,0 +1,16 @@
+
+
+class G {
+
+        public static void main(String[] args){
+
+                System.out.println(Math.PI);
+                System.out.println(Math.E);
+        }
+}
+
+// :\javaprac\9_import>javac G.java
+
+// D:\javaprac\9_import>java G
+// 3.141592653589793
+// 2.718281828459045
