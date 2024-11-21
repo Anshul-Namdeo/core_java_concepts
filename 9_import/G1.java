2cfa947a7842eecf6e95742402334aeeaa3a0911
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-11-21 15:49:10 +0530

    import

---
+++ 9_import/G1.java
@@ -0,0 +1,36 @@
+
+
+class G1 {
+
+        public static void main(String [] args){
+
+                System.out.println(PI);
+                System.out.println(E);
+
+                D:\javaprac\9_import>javac G1.java
+G1.java:7: error: cannot find symbol
+                System.out.println(PI);
+                                   ^
+  symbol:   variable PI
+  location: class G1
+G1.java:8: error: cannot find symbol
+                System.out.println(E);
+                                   ^
+  symbol:   variable E
+  location: class G1
+2 errors
+        }
+}
+
+// D:\javaprac\9_import>javac G1.java
+// G1.java:7: error: cannot find symbol
+//                 System.out.println(PI);
+//                                    ^
+//   symbol:   variable PI
+//   location: class G1
+// G1.java:8: error: cannot find symbol
+//                 System.out.println(E);
+//                                    ^
+//   symbol:   variable E
+//   location: class G1
+// 2 errors
