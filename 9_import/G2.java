39c200acac95e31331afb73ea89600af23c6b448
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-11-21 15:49:44 +0530

    import

---
+++ 9_import/G2.java
@@ -0,0 +1,17 @@
+import static java.lang.Math.PI;
+import static java.lang.Math.E;
+
+class G2{
+
+        public static void main(String[] args){
+
+                System.out.println(PI);
+                System.out.println(E);
+        }
+}
+
+// :\javaprac\9_import>javac G2.java
+
+// D:\javaprac\9_import>java G2
+// 3.141592653589793
+// 2.71828182845904
