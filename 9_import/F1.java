5799c6a0123e8d4b4a9c4db3506dd9030364d89b
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-11-21 15:46:17 +0530

    import

---
+++ 9_import/F1.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+
+class F1 {
+
+        public static void main(String[] args){
+
+                Date x;
+                File y;
+                Format Z ;
+        }
+}
+
+// F1.java:8: error: cannot find symbol
+//                 File y;
+//                 ^
+//   symbol:   class File
+//   location: class F1
+// F1.java:9: error: cannot find symbol
+//                 Format Z ;
+//                 ^
+//   symbol:   class Format
+//   location: class F1
+// 2 errors
