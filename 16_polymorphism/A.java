1770871e249b4edcaafda7080a4f70d05b95d0aa
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-12-13 22:45:52 +0530

    basic code of polymorphism

---
+++ 16_polymorphism/A.java
@@ -0,0 +1,23 @@
+class A {
+
+}
+class B extends A {
+
+}
+class C extends A{
+
+}
+
+class Text {
+
+        public static void main(String[] args){
+
+                C x  = new C();
+
+                B y  = new B();
+
+                A z  = new A();
+
+                Object t = new Object();
+        }
+}
