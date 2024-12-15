1769e0a1ad6f069a1424a56dd241d39dd895cb4e
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-12-15 18:32:22 +0530

    done

---
+++ 16_polymorphism/B.java
@@ -0,0 +1,11 @@
+class B {
+    public static void main(String[] args){
+
+            Object  a  =  new Object();
+            Object b = new A();
+            Object c = new B();
+            Object d = new C();
+    }
+}
+
+// cpmpile A.java the B.java
