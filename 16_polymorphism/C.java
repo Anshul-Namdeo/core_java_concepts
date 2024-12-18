5c17e2a109491032e5b4fc626444d8ad74ae463e
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-12-18 23:43:14 +0530

    basic polymorphism code

---
+++ 16_polymorphism/C.java
@@ -0,0 +1,21 @@
+class Plant/* extends Object*/ {
+
+}
+class Machine /* extends Object */{
+
+}
+class Human /* extends Object */{
+
+}
+
+class C {
+
+        public static void main(String[] args){
+
+                Object x = new Plant();
+
+                Object y = new Machine();
+
+                Object z = new Human();
+        }
+}
