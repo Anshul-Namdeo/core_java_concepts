25132ea666f6ca6e9dfac12dff0b554ee0b1a609
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-12-26 22:51:11 +0530

    two same name & diff. parameterized methods are present in the class

---
+++ 17_overloading/B.java
@@ -0,0 +1,14 @@
+class B {
+
+        void pro(){
+
+        }
+
+        void pro(int a){
+
+        }
+}
+
+// parameter body must mismatch in all version of overloaded method.
+
+// parameter count mismatch in above example.
