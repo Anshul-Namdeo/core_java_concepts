45ffaf7d868a47d2a47771ccb5032c0f425830cd
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-12-26 22:58:35 +0530

    two same name & two same  parameterized methods are present in the class

---
+++ 17_overloading/C.java
@@ -0,0 +1,15 @@
+class C {
+
+        void pro(int a, float b){
+
+        }
+
+        void pro(int a, float b){
+
+        }
+}
+
+// C.java:7: error: method pro(int,float) is already defined in class C
+//         void pro(int a, float b){
+//              ^
+// 1 error
