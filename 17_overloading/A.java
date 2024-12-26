7ff5c1b6119d0b132f4c6103bda35634246bde4c
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-12-26 22:48:09 +0530

    two same name & same parameterized methods are present in the class

---
+++ 17_overloading/A.java
@@ -0,0 +1,15 @@
+class A {
+
+        void pro(){
+
+        }
+
+        void pro(){
+
+        }
+}
+
+// A.java:7: error: method pro() is already defined in class A
+//         void pro(){
+//              ^
+// 1 error
