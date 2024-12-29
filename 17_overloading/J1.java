46216c0162385a0605c31079fa57892e71b69cab
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-12-29 20:00:44 +0530

    Demonstrated Math.min with integer, long, float, and double values.

---
+++ 17_overloading/J1.java
@@ -0,0 +1,11 @@
+class J1{
+
+        public static void main(String args[]){
+
+                System.out.println(Math.min(42,36));
+                 System.out.println(Math.min(4245698745632L,452123665478936L));
+                  System.out.println(Math.min(4.22525f,3.62121f));
+                   System.out.println(Math.min(4.2,5.2));
+
+        }
+}
