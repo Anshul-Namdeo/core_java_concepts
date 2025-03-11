e6a7464479904ac86b4938b887cd04e62c8362f3
Anshul_Namdeo <anshulnamdeo280@gmail.com>
2025-03-11 19:02:48 +0530

    Delete 24_arrays directory

--- 24_arrays/E.java
+++
@@ -1,9 +0,0 @@
-class E {
-    public static void main(String args[]){
-
-            int[] x = new int[-1];
-    }
-}
-
-// Exception in thread "main" java.lang.NegativeArraySizeException: -1
-//         at E.main(E.java:4)
--- 24_arrays/X.java
+++
@@ -1,7 +0,0 @@
-class X{
-    public static void main(String args[]){
-        System.out.println(args);
-    }
-}
-
-// [Ljava.lang.String;@4617c264
