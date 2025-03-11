311ad276f4e74a3e384a2e7dbf0c0fedb0472841
Anshul_Namdeo <anshulnamdeo280@gmail.com>
2025-03-11 19:04:29 +0530

    Delete 33_exception_handling directory

--- 33_exception_handling/W2.java
+++
@@ -1,7 +0,0 @@
-class W2 extends RuntimeException
-{
-    public static void main(String[] args)  {
-        
-        throw new W2();
-    }
-}
--- 33_exception_handling/W3.java
+++
@@ -1,7 +0,0 @@
-class W3 extends Throwable
-{
-    public static void main(String[] args) throws Throwable {
-        
-        throw new W3();
-    }
-}
