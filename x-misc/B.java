451730bb8609658f7a943a548d88299ce0a71d9a
Anshul_Namdeo <anshulnamdeo280@gmail.com>
2025-03-11 19:09:00 +0530

    Delete x-misc directory

--- x-misc/B.java
+++
@@ -1,9 +0,0 @@
-class B {
-    public static void main(String args[]){
-
-        Teachar t = new Teachar("manoj" , 21);
-
-        System.out.println(t);
-        System.out.println(t.toString());
-    }
-}
